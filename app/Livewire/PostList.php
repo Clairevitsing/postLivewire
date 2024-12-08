03a85fbf71dc0b712af71d6137c8ef557b2eb251
claire <vitsingz@gmail.com>
2024-12-08 21:47:19 +0100

    display posts list

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -3,11 +3,18 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class PostList extends Component
 {
+    public $posts;
+    public function mount(){
+        $this->posts = Post::all();
+
+    }
     public function render()
     {
+        // dd($this->posts);
         return view('livewire.post-list');
     }
 }
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -3,20 +3,66 @@
         <div class="col-xl-11">
             <h4 class="text-center fw-bold"> SPA - CRUD App Using Livewire and Laravel11</h4>
         </div>
-    <div class="col-xl-1">
+        <div class="col-xl-1">
             <a wire:navigate href="{{ route('posts.create') }}" class="btn btn-primary btn-sm"> Add Post</a>
         </div>
     </div>
 
-    @if (session('success'))
-         <div class="alert alert-success alert-dismissible">
+    <div class="my-2">
+        @if (session('success'))
+        <div class="alert alert-success alert-dismissible">
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-              {{ session('success') }}
-         </div>
-    @elseif (session('error'))
-     <div class="alert alert-danger alert-dismissible">
+            {{ session('success') }}
+        </div>
+        @elseif (session('error'))
+        <div class="alert alert-danger alert-dismissible">
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-              {{ session('error') }}
-         </div>
-    @endif
+            {{ session('error') }}
+        </div>
+        @endif
+
+    </div>
+
+
+    <div class="card shadow">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thread>
+                    <th>#</th>
+                    <th>Featured Image</th>
+                    <th>Title</th>
+                    <th>Content</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </thread>
+
+                </tabl>
+
+                <tbody>
+
+                    @forelse ($posts as $post)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td> <img src="{{ Storage::url($post->featured_image) }}" class="img-fluid" width="200px"></td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->content }}</td>
+                        <td>
+                            <p><small><strong>Posted: </strong>{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</small></p>
+                            <p><small><strong>Last Updated: </strong>{{ \Carbon\Carbon::parse($post->updated_at)->diffForHumans() }}</small></p>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-success btn-sm">Edit</a>
+                            <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                        </td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="6" class="text-center">No posts found.</td>
+                    </tr>
+                    @endforelse
+
+                </tbody>
+        </div>
+    </div>
+
 </div>
